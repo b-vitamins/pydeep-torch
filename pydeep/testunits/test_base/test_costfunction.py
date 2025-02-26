465a031e699c87145759a892448224d6f4149352
Ayan Das <bvits@riseup.net>
2025-02-26 23:35:30 +0530

    refactor: testunits: Refactor test_costfunction.py.

--- pydeep/testunits/test_base/test_costfunction.py
+++ pydeep/testunits/test_base/test_costfunction.py
@@ -35,7 +35,7 @@
 
 import unittest
 from pydeep.base.costfunction import *
-import numpy as numx
+import numpy as np
 import sys
 
 print("\n... pydeep.base.costfunction.py")
@@ -47,79 +47,77 @@ class TestCostFunction(unittest.TestCase):
     def test_MeanSquaredError(self):
         sys.stdout.write("CostFunction -> Performing MeanSquaredError test ...")
         sys.stdout.flush()
-        x = numx.array([[0.0, -92.0, 42.0, 87.0]])
-        t = numx.array([[0.00000001, 2, 4.0, 86.9999]])
-        assert numx.all(numx.abs(5140.00000001 - SquaredError.f(x, t)) < epsilon)
+        x = np.array([[0.0, -92.0, 42.0, 87.0]])
+        t = np.array([[0.00000001, 2, 4.0, 86.9999]])
+        assert np.all(np.abs(5140.00000001 - SquaredError.f(x, t)) < epsilon)
         res = [[-1.00000000e-08, -9.40000000e01, 3.80000000e01, 1.00000000e-04]]
-        assert numx.all(numx.abs(res - SquaredError.df(x, t)) < epsilon)
+        assert np.all(np.abs(res - SquaredError.df(x, t)) < epsilon)
 
-        x = numx.array([[0, 1, 2, 3]])
-        t = numx.array([[0, 1, 2, 3]])
-        assert numx.all(numx.abs(0.0 - SquaredError.f(x, t)) < epsilon)
-        assert numx.all(numx.abs(0.0 - SquaredError.df(x, t)) < epsilon)
+        x = np.array([[0, 1, 2, 3]])
+        t = np.array([[0, 1, 2, 3]])
+        assert np.all(np.abs(0.0 - SquaredError.f(x, t)) < epsilon)
+        assert np.all(np.abs(0.0 - SquaredError.df(x, t)) < epsilon)
         print(" successfully passed!")
         sys.stdout.flush()
 
     def test_MeanAbsoluteError(self):
         sys.stdout.write("CostFunction -> Performing MeanAbsoluteError test ...")
         sys.stdout.flush()
-        x = numx.array([[0.0, -92.0, 42.0, 87.0]])
-        t = numx.array([[0.00000001, 2, 4.0, 86.9999]])
-        assert numx.all(numx.abs(132.00010001 - AbsoluteError.f(x, t)) < epsilon)
+        x = np.array([[0.0, -92.0, 42.0, 87.0]])
+        t = np.array([[0.00000001, 2, 4.0, 86.9999]])
+        assert np.all(np.abs(132.00010001 - AbsoluteError.f(x, t)) < epsilon)
         res = [[-1.0, -1.0, 1.0, 1.0]]
-        assert numx.all(numx.abs(res - AbsoluteError.df(x, t)) < epsilon)
+        assert np.all(np.abs(res - AbsoluteError.df(x, t)) < epsilon)
 
-        x = numx.array([[0, 1, 2, 3]])
-        t = numx.array([[0, 1, 2, 3]])
-        assert numx.all(numx.abs(0.0 - AbsoluteError.f(x, t)) < epsilon)
-        assert numx.all(numx.abs(0.0 - AbsoluteError.df(x, t)) < epsilon)
+        x = np.array([[0, 1, 2, 3]])
+        t = np.array([[0, 1, 2, 3]])
+        assert np.all(np.abs(0.0 - AbsoluteError.f(x, t)) < epsilon)
+        assert np.all(np.abs(0.0 - AbsoluteError.df(x, t)) < epsilon)
         print(" successfully passed!")
         sys.stdout.flush()
 
     def test_CrossEntropyError(self):
         sys.stdout.write("CostFunction -> Performing CrossEntropyError test ...")
         sys.stdout.flush()
-        x = numx.array([[0.5, 0.01, 0.99]])
-        t = numx.array([[0.0, 0.0, 1.0]])
-        assert numx.all(numx.abs(0.713247852267 - CrossEntropyError.f(x, t)) < epsilon)
-        res = numx.array([[2.0, 1.01010101, -1.01010101]])
-        assert numx.all(numx.abs(res - CrossEntropyError.df(x, t)) < epsilon)
+        x = np.array([[0.5, 0.01, 0.99]])
+        t = np.array([[0.0, 0.0, 1.0]])
+        assert np.all(np.abs(0.713247852267 - CrossEntropyError.f(x, t)) < epsilon)
+        res = np.array([[2.0, 1.01010101, -1.01010101]])
+        assert np.all(np.abs(res - CrossEntropyError.df(x, t)) < epsilon)
 
-        x = numx.array([[0.5, 0.01, 0.99]])
-        t = numx.array([[1.0, 1.0, 0.0]])
-        assert numx.all(numx.abs(9.90348755254 - CrossEntropyError.f(x, t)) < epsilon)
-        res = numx.array([[-2.0, -100.0, 100.0]])
-        assert numx.all(numx.abs(res - CrossEntropyError.df(x, t)) < epsilon)
-        x = numx.array([[0.0000000001, 0.9999999999]])
-        t = numx.array([[0.0, 1.0]])
-        assert numx.all(
-            numx.abs(2.00000016558e-10 - CrossEntropyError.f(x, t)) < epsilon
-        )
+        x = np.array([[0.5, 0.01, 0.99]])
+        t = np.array([[1.0, 1.0, 0.0]])
+        assert np.all(np.abs(9.90348755254 - CrossEntropyError.f(x, t)) < epsilon)
+        res = np.array([[-2.0, -100.0, 100.0]])
+        assert np.all(np.abs(res - CrossEntropyError.df(x, t)) < epsilon)
+        x = np.array([[0.0000000001, 0.9999999999]])
+        t = np.array([[0.0, 1.0]])
+        assert np.all(np.abs(2.00000016558e-10 - CrossEntropyError.f(x, t)) < epsilon)
 
-        x = numx.array([[0.0000000001, 0.9999999999]])
-        t = numx.array([[0.0, 1.0]])
-        res = numx.array([[1.0, -1.0]])
-        assert numx.all(numx.abs(res - CrossEntropyError.df(x, t)) < epsilon)
+        x = np.array([[0.0000000001, 0.9999999999]])
+        t = np.array([[0.0, 1.0]])
+        res = np.array([[1.0, -1.0]])
+        assert np.all(np.abs(res - CrossEntropyError.df(x, t)) < epsilon)
         print(" successfully passed!")
         sys.stdout.flush()
 
     def test_NegLogLikelihood(self):
         sys.stdout.write("CostFunction -> Performing NegLogLikelihood test ...")
         sys.stdout.flush()
-        x = numx.array([[0.5, 0.01, 0.99]])
-        t = numx.array([[0.0, 0.0, 1.0]])
-        assert numx.all(numx.abs(0.01005034 - NegLogLikelihood.f(x, t)) < epsilon)
-        res = numx.array([[0.0, 0.0, -1.01010101]])
-        assert numx.all(numx.abs(res - NegLogLikelihood.df(x, t)) < epsilon)
+        x = np.array([[0.5, 0.01, 0.99]])
+        t = np.array([[0.0, 0.0, 1.0]])
+        assert np.all(np.abs(0.01005034 - NegLogLikelihood.f(x, t)) < epsilon)
+        res = np.array([[0.0, 0.0, -1.01010101]])
+        assert np.all(np.abs(res - NegLogLikelihood.df(x, t)) < epsilon)
 
-        x = numx.array([[0.5, 0.01, 0.99]])
-        t = numx.array([[1.0, 0.0, 0.0]])
-        assert numx.all(numx.abs(0.69314718 - NegLogLikelihood.f(x, t)) < epsilon)
-        res = numx.array([[-2.0, 0.0, 0.0]])
-        assert numx.all(numx.abs(res - NegLogLikelihood.df(x, t)) < epsilon)
+        x = np.array([[0.5, 0.01, 0.99]])
+        t = np.array([[1.0, 0.0, 0.0]])
+        assert np.all(np.abs(0.69314718 - NegLogLikelihood.f(x, t)) < epsilon)
+        res = np.array([[-2.0, 0.0, 0.0]])
+        assert np.all(np.abs(res - NegLogLikelihood.df(x, t)) < epsilon)
         print(" successfully passed!")
         sys.stdout.flush()
 
 
-if __name__ is "__main__":
+if __name__ == "__main__":
     unittest.main()
